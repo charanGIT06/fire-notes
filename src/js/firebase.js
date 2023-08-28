2a4d805ca3b606ef7141dd2db902a27e1883a9f3
Charan K <96834040+charanGIT06@users.noreply.github.com>
2023-08-28 16:41:07 +0530

    emulator bug fix

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -14,10 +14,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+// connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
 
 const db = getFirestore(app);
-connectFirestoreEmulator(db, "127.0.0.1", 8080);
+// connectFirestoreEmulator(db, "127.0.0.1", 8080);
 
 const firebase = {
   app,
