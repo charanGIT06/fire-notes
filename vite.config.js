4197776014586baa4c9e47904a5bf444991e7d31
Charan K <96834040+charanGIT06@users.noreply.github.com>
2023-08-29 17:28:50 +0530

    Manifest Bug Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
-import { GiBison } from "react-icons/gi";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -75,7 +74,7 @@ export default defineConfig({
             url: "/shared",
             icons: [
               {
-                src: { GiBison },
+                src: "/icons/fire-192.png",
                 sizes: "192x192",
                 type: "image/png",
               },
